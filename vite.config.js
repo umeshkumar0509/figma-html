c08dfc276378161de2f652385af309a789c3cda3
Umesh Kumar <54438526+umeshkumar0509@users.noreply.github.com>
2026-07-17 14:25:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  // GitHub Pages root deployment
+  plugins: [react(), tailwindcss()],
   base: '/figma-html/',
-
-  server: {
-    port: 5173,
-  },
-
-  build: {
-    outDir: 'dist',
-  },
 })
